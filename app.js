7b4cab503233fc41a766444765901d7fdb8d640f
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-13 15:22:24 -0300

    Core game logic

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
 
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS"
+const RESULT_COMPUTER_WINS = "COMPUTER WINS"
+
+
 let gameIsRunning = false;
 
 
@@ -17,12 +22,38 @@ const getPlayerChoice = function () {
     return selection;
 };
 
+function getComputerChoice (){
+    const randomValue = Math.random(); //between 0 and 1
+    if( randomValue < 0.34 ){
+        return ROCK;
+    } else if ( randomValue < 0.67 ){
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+function getWinner ( computerChoice, playerChoice ){
+    if(computerChoice === playerChoice){
+        return RESULT_DRAW;
+    } else if ( (computerChoice === ROCK && playerChoice === PAPER) || 
+                (computerChoice === PAPER && playerChoice === SCISSORS) ||
+                (computerChoice === SCISSORS && playerChoice === ROCK) ) {
+                    return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+}
+
+
 startGameBtn.addEventListener("click", function startAnonFunc(){
     if(gameIsRunning){
         return;
     }
     gameIsRunning = true;
     console.log("Game is starting...");
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice,playerChoice)
+    console.log(winner);
 });
