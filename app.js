33a420c27a8104cf7048b015910777afcf9596f4
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-13 15:06:57 -0300

    Adding user choice to a game

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+
+const getPlayerChoice = function () {
+    const selection = prompt("Rock, Paper or Scissors?","").toUpperCase(); //converting all to uppercase (matching the consts)
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS){
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you.`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
+
+startGameBtn.addEventListener("click", function startAnonFunc(){
+    if(gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
+    console.log("Game is starting...");
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
